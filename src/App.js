a9bfb8405d38e4223f0e5b0e5d8f6903f3aabf77
iwasaki-hub <66316321+iwasaki-hub@users.noreply.github.com>
2024-11-16 14:58:50 +0900

    change home route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/homepage">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
