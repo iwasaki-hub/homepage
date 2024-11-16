1aaf433af31237016134eeef5e06b52dc749ce51
iwasaki-hub <66316321+iwasaki-hub@users.noreply.github.com>
2024-11-16 14:55:37 +0900

    change home route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <>
+    <Router basename="/">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
